e9d6136829329a1194d8b29f9473b40b481c39b2
Tommy Condon <114081733+tommyc2@users.noreply.github.com>
2025-02-11 14:29:34 +0000

    Refactor UI to make it more friendly for kids

--- main.ts
+++ main.ts
@@ -4,35 +4,83 @@
  * See if you can spot them!
  */
 /**
- * When A is pressed down, the car goes left
+ * l = left
  * 
- * When B is pressed down, the car goes right
+ * r = right
+ * 
+ * s = stop
+ * 
+ * f = both motors (left and right) go forward
+ */
+/**
+ * When A is pressed down, the car goes LEFT
+ * 
+ * When B is pressed down, the car goes RIGHT
  * 
  *  
  * 
- * When either button (A or B) is released, the car stops
- */
-/**
- * See if you can add more improvements here!
+ * When either button (A or B) is released, the car STOPS
  * 
- * See if you can make the car reverse!
+ * When both buttons (A & B) are pressed together, the car STOPS
  */
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
     radio.sendValue("s", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
     radio.sendValue("s", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # . . . .
+        `)
     radio.sendValue("f", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
     radio.sendValue("l", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
     radio.sendValue("s", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    basic.showLeds(`
+        # # # # .
+        # . . . #
+        # # # # .
+        # . . . #
+        # . . . #
+        `)
     radio.sendValue("r", 0)
 })
 radio.setGroup(2)

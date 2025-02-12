3959e001cad4a5c1fbfbb06490f87def1b43da5b
Tommy Condon <114081733+tommyc2@users.noreply.github.com>
2025-02-12 22:29:09 +0000

    v1

--- main.ts
+++ main.ts
@@ -12,76 +12,29 @@
  * 
  * f = both motors (left and right) go forward
  */
-/**
- * When A is pressed down, the car goes LEFT
- * 
- * When B is pressed down, the car goes RIGHT
- * 
- *  
- * 
- * When either button (A or B) is released, the car STOPS
- * 
- * When both buttons (A & B) are pressed together, the car STOPS
- */
+// When A is pressed down, the car goes LEFT
+// 
+// When B is pressed down, the car goes RIGHT
+// 
+// 
+// 
+// When either button (A or B) is released, the car STOPS
+// 
+// When both buttons (A & B) are pressed together, the car STOPS
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
-    basic.showLeds(`
-        # # # # #
-        # . . . .
-        # # # # #
-        . . . . #
-        # # # # #
-        `)
-    radio.sendValue("s", 0)
-})
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
-    basic.showLeds(`
-        # # # # #
-        # . . . .
-        # # # # #
-        . . . . #
-        # # # # #
-        `)
-    radio.sendValue("s", 0)
+    radio.sendValue("STOP", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_AB, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    basic.showLeds(`
-        # # # # #
-        # . . . .
-        # # # # #
-        # . . . .
-        # . . . .
-        `)
-    radio.sendValue("f", 0)
+    radio.sendValue("FORWARD", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    basic.showLeds(`
-        # . . . .
-        # . . . .
-        # . . . .
-        # . . . .
-        # # # # #
-        `)
-    radio.sendValue("l", 0)
+    radio.sendValue("LEFT", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
-    basic.showLeds(`
-        # # # # #
-        # . . . .
-        # # # # #
-        . . . . #
-        # # # # #
-        `)
-    radio.sendValue("s", 0)
+    radio.sendValue("STOP", 0)
 })
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    basic.showLeds(`
-        # # # # .
-        # . . . #
-        # # # # .
-        # . . . #
-        # . . . #
-        `)
-    radio.sendValue("r", 0)
+    radio.sendValue("RIGHT", 0)
 })
 radio.setGroup(2)
 motobit.enable(MotorPower.On)
